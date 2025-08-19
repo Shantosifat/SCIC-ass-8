89e817dc87152a8fffc689ae44686a07cd9e3410
Shantosifat1 <shanto201934024@gmail.com>
2025-08-19 13:52:55 +0600

    5 prblm done

--- JS assignment.js
+++ JS assignment.js
@@ -21,4 +21,35 @@ function countVowels(str) {
 
 console.log(countVowels("programming")); // 3
 
+// problem 3
+
+function isPalindrome(str) {
+  let reversed = str.split("").reverse().join("");
+  return str === reversed;
+}
+
+console.log(isPalindrome("madam")); // true
+console.log(isPalindrome("hello")); // false
+
+// problem 4
+
+function findLargest(arr) {
+  return Math.max(...arr);
+}
+
+console.log(findLargest([5, 1, 9, 3])); // 9
+
+// problem 5
+
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
+}
+
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4])); // [1, 2, 3, 4]
+
+// problem 4
+// problem 4
+// problem 4
+// problem 4
+// problem 4
 
